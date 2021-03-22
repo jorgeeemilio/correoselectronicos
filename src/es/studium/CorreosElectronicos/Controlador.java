0226b041145f0b37362347dc93d58ea48153a16f
Jorge Rodríguez <jorge@grupostudium.com>
2021-03-22 10:53:36 +0100

    Integración del Modelo

--- src/es/studium/CorreosElectronicos/Controlador.java
+++ src/es/studium/CorreosElectronicos/Controlador.java
@@ -4,15 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Controlador implements WindowListener, ActionListener
 {
@@ -21,8 +12,8 @@ public class Controlador implements WindowListener, ActionListener
 
 	public Controlador(Vista v, Modelo m)
 	{
-		vista = v;
-		modelo = m;
+		this.vista = v;
+		this.modelo = m;
 		vista.ventana.addWindowListener(this);
 		vista.dlgMensaje.addWindowListener(this);
 		vista.btnAnalizar.addActionListener(this);
@@ -33,67 +24,18 @@ public class Controlador implements WindowListener, ActionListener
 	{
 		if(evento.getSource().equals(vista.btnAnalizar))
 		{
-			// Leer el fichero "correoselectronicos.txt"
-			// Analizar los correos
-			// Guardar en "correctos.txt"
-			Pattern correos;
-			Matcher m;
-			correos = Pattern.compile("[a-z0-9._]{2,126}@[a-z0-9.]{2,126}.[a-z0-9]{2,126}");
-
-			// Leer fichero
-			try
+			if((vista.txtFuente.getText().length()!=0)&&(vista.txtDestino.getText().length()!=0))
 			{
-				//Origen de los datos en el proyecto anterior
-				FileReader fr = new FileReader(vista.txtRuta.getText());
-				//Buffer de lectura
-				BufferedReader entrada = new BufferedReader(fr);
-				// Destino de los datos
-				FileWriter fw = new FileWriter("correctos.txt", true);
-				// Buffer de escritura
-				BufferedWriter bw = new BufferedWriter(fw);
-				// Objeto para la escritura
-				PrintWriter salida = new PrintWriter(bw);
-
-				String s;
-				//Bucle para sacar la información del archivo
-				while((s=entrada.readLine())!=null)
-				{
-					vista.txaResultados.append(s);
-					m = correos.matcher(s);
-					if(!m.matches())
-					{
-						vista.txaResultados.append("-->Incorrecto");
-					}
-					else
-					{
-						vista.txaResultados.append("-->Correcto");
-						salida.println(s);
-					}
-					vista.txaResultados.append("\n");
-				}
-				vista.lblMensaje.setText("Proceso finalizado");
-				//Cerrar el objeto entrada
-				entrada.close();
-				fr.close();
-				//Cerrar el objeto salida, el objeto bw y el fw
-				salida.close();
-				bw.close();
-				fw.close();
+				vista.txaResultados.selectAll();
+				vista.txaResultados.setText("");
+				modelo.analizar(vista);
 			}
-			catch(FileNotFoundException e)
+			else
 			{
-				vista.lblMensaje.setText("Archivo NO encontrado");
-			}
-			catch(IOException i)
-			{
-				vista.lblMensaje.setText("Se produjo un error de Archivo");
-			}
-			finally
-			{
+				vista.lblMensaje.setText("Debes indicar los nombres de los ficheros");
 				vista.dlgMensaje.setVisible(true);
 			}
 		}
-
 	}
 
 	@Override
